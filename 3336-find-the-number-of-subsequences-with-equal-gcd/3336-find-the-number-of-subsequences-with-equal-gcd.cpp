f6696612cd712b3ceeb96efc3bb23d217a401c9d
Pranjal Srivastava <pranjalsrivastava2026@gmail.com>
2026-07-14 15:29:21 +0530

    Time: 436 ms (72.17%), Space: 290.5 MB (45.22%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -11,20 +11,19 @@ class Solution {
  private:
   static constexpr int kMod = 1'000'000'007;
 
-  // Returns the number of disjoint pairs `seq1` and `seq2` of nums[i..n - 1],
-  // where GCD(seq1) == x and GCD(seq2) == y.
+  
   int subsequencePairCount(const vector<int>& nums, int i, int x, int y,
                            vector<vector<vector<int>>>& mem) {
     if (i == nums.size())
       return x > 0 && x == y;
     if (mem[i][x][y] != -1)
       return mem[i][x][y];
-    // 1. Skip nums[i].
+    
     const int skip = subsequencePairCount(nums, i + 1, x, y, mem);
-    // 2. Pick nums[i] in the first subsequence.
+    
     const int take1 =
         subsequencePairCount(nums, i + 1, gcd(x, nums[i]), y, mem);
-    // 3. Pick nums[i] in the second subsequence.
+   
     const int take2 =
         subsequencePairCount(nums, i + 1, x, gcd(y, nums[i]), mem);
     return mem[i][x][y] = (static_cast<long>(skip) + take1 + take2) % kMod;
