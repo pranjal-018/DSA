34909a52a34f7f30082f2ab6748c380d3907b6ff
Pranjal Srivastava <pranjalsrivastava2026@gmail.com>
2026-09-17 16:37:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (20.45%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,26 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        int reversedNumber = 0;
-      
-        // Process each digit of the input number
-        while (x != 0) {
-            // Check for potential overflow before multiplying by 10
-            // INT_MAX = 2147483647, INT_MIN = -2147483648
-            // If reversedNumber > INT_MAX/10, then reversedNumber * 10 will overflow
-            // If reversedNumber < INT_MIN/10, then reversedNumber * 10 will underflow
-            if (reversedNumber > INT_MAX / 10 || reversedNumber < INT_MIN / 10) {
-                return 0;
-            }
-          
-            // Extract the last digit and append it to the reversed number
-            int lastDigit = x % 10;
-            reversedNumber = reversedNumber * 10 + lastDigit;
-          
-            // Remove the last digit from the original number
-            x /= 10;
+        int ans = 0;
+      while (x != 0) {
+            
+        if (ans > INT_MAX / 10 ||ans < INT_MIN / 10) {
+            return 0;
+        }
+        int Digit = x % 10;
+        ans = ans * 10 + Digit;
+        x = x/10;
         }
       
-        return reversedNumber;
+        return ans;
     }
 };
