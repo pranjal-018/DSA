7c237f3eff0118a022b5f5b4b65c997799309656
Pranjal Srivastava <pranjalsrivastava2026@gmail.com>
2026-09-18 17:16:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (81.59%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -12,8 +12,11 @@ public:
         return false;
     }
 };
-*/ 
+*/
+
 // 2 approach to solve a problem 
+
+/*
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
@@ -26,3 +29,23 @@ public:
         return false;
     }
 };
+*/
+
+// 3 approach to solve a problem which is efficient approach the first two is brute force approach
+
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        int ans=1;
+        for (int i=0; i<=30; i++){
+        if (ans==n){
+            return true;
+        }
+        if(ans<INT_MAX/2){
+        ans= ans * 2;
+        }
+    }
+        return false;
+    }
+};
+
